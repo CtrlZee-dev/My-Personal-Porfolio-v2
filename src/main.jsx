6346c1f65390d0d2af8032a5d283912a71b92473
Zizipho <ziziphokakaza09@gmail.com>
2025-12-09 15:00:36 +0200

    main fix 2

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter base="/My-Personal-Porfolio-v2/">
+    <BrowserRouter base="/My-Personal-Porfolio-v2">
       <App />
     </BrowserRouter>
   </StrictMode>
